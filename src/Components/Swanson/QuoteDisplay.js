639ce7c19ffe3b7872ff3a1fa16acce9b1fbc14b
brynbaxter <brynmcivor1@gmail.com>
2021-01-27 23:49:28 +1300

    end of video series

--- src/Components/Swanson/QuoteDisplay.js
+++ src/Components/Swanson/QuoteDisplay.js
@@ -1,9 +1,10 @@
-const QuoteDisplay = ({ quote, author }) => {
+const QuoteDisplay = ({ quote, author, updateQuote }) => {
   return (
     <div>
       <h2>Random Swanson Quote</h2>
-      <p id="text">{quote}</p>
+      <p id="text">"{quote}"</p>
       <p id="author">~{author}</p>
+      <button children={'New Quote'} onClick={updateQuote} />
     </div>
   );
 };
--- src/Components/Swanson/SwansonQuotes.js
+++ src/Components/Swanson/SwansonQuotes.js
@@ -1,36 +1,34 @@
-import useFetch from '../../useFetch';
 import QuoteDisplay from './QuoteDisplay';
 import { useState, useEffect } from 'react';
 
 const SwansonQuotes = () => {
-  const { data: quote, isPending, error } = useFetch(
-    'https://ron-swanson-quotes.herokuapp.com/v2/quotes'
-  );
-
+  const [isPending, setIsPending] = useState(true);
   const url = 'https://ron-swanson-quotes.herokuapp.com/v2/quotes';
-  const [quoteA, setQuote] = useState('');
+  const [quote, setQuote] = useState('');
 
   const updateQuote = async () => {
+    setIsPending(true);
     const response = await fetch(url);
     const data = await response.json();
     setQuote(data[0]);
+    setIsPending(false);
   };
 
   useEffect(() => {
     updateQuote();
     console.log('This happens on mount');
-
-    return () => {
-      console.log('This happens on unmount');
-    };
   }, []);
 
   return (
     <div id="quote-box" className="home">
-      {error && <div>{error}</div>}
+      {quote && (
+        <QuoteDisplay
+          quote={quote}
+          author="Ron Swanson"
+          updateQuote={updateQuote}
+        />
+      )}
       {isPending && <div>Loading...</div>}
-      {quote && <QuoteDisplay quote={quoteA} author="Ron Swanson" />}
-      {quoteA && <button children={'New Quote'} onClick={updateQuote} />}
     </div>
   );
 };
